d79bce7ea6dbb46feca11bf22be7f3e4eb80d991
ruvido <ruvido@gmail.com>
2018-10-12 10:04:13 +0200

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
-txt=txt
-posts=_posts
-
-drive pull -no-prompt -export txt -exports-dir txt -explicitly-export -same-exports-dir ../date
-
-if [ ! -d $posts ]; then mkdir $posts; fi
-cd txt
-for ii in *; do
-    file=${ii%.*} 
-    cp $ii ../$posts/$file.md; 
-done
+git add .
+git commit -a -m 'commit'
+git pull
+git push
